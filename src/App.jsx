4dc7511aebfd84a2f4a1b8d73c991cecec9ec7eb
Harsh260105 <harshpatel17079@gmail.com>
2024-07-29 09:40:27 +0530

    routing modification

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,17 @@ import Navbar from './components/Navbar';
 const App = () => {
   return (
     <main className='bg-slate-300/20 h-full'>
-        <Router>
+        <Router basename="/harsh-patel-portfolio/">
             <Navbar />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
-                <Route path='/Projects' element={<Projects />} />
-                <Route path='/Contact' element={<Contact />} />
+                <Route path='/projects' element={<Projects />} />
+                <Route path='/contact' element={<Contact />} />
             </Routes>
         </Router>
     </main>
   )
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/harsh-patel-portfolio/",
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
