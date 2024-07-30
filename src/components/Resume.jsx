f42769e08880c2052fa0e3ae2e9de33590da49ae
Harsh260105 <harshpatel17079@gmail.com>
2024-07-30 13:15:34 +0530

    path changes

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,7 +7,7 @@ const Resume = () => {
                 Take a look at my resume!
             </p>
             <a
-                href={"public/HARSH-PATEL-Resume.pdf"}
+                href={"/HARSH-PATEL-Resume.pdf"}
                 download={"HARSH-PATEL-Resume.pdf"}
                 className="btn"
             > Download </a>
