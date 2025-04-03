3e612f716f5d6730541c6632ecbcc9baf238f2a1
Harsh260105 <harshpatel17079@gmail.com>
2025-04-04 00:24:45 +0530

    Add Vercel Analytics integration and update package dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import ReactDom from 'react-dom/client';
 import App from './App';
 import "./index.css";
+import { Analytics } from '@vercel/analytics/react';
 
-ReactDom.createRoot(document.getElementById('root')).render(<App />);
+ReactDom.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
